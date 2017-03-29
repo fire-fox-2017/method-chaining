5eb4b0ba1ded4bb8a5a9e392683d69dbb0173289
fearzen3 <ivanhabi2@gmail.com>
2017-03-29 21:30:07 +0700

    selse

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -18,19 +18,52 @@ let data = [{
 
 // Our object with the chainable methods using class in ES6
 class Program {
-  constructor () {
+  constructor (input) {
+    this.data=input;
+    this.SearchName='';
+    this.searchFName='';
+    this.searchLName='';
+    this.searchID='';
+    this.searchEmail='';
+    this.tampil='';
   }
   titleCaseName (string) {
+
+
+
   }
   findUser (email) {
+    for(let i=0;i<this.data.length;i++){
+
+      if(email===this.data[i].email){
+      this.searchFName=this.data[i].firstName;
+      this.searchLName=this.data[i].lastName;
+      this.searchID=this.data[i].id;
+      this.searchEmail=this.data[i].email;
+      }
+
+    }
+    return this;
   }
+
   formatName () {
+    this.SearchName = this.searchFName+' '+this.searchLName;
+
+    return this;
   }
   formatData () {
+    this.tampil = 'Hasil : \n Member name: '+ this.SearchName +' \n ID : '+ this.searchID+ '\n Email:'+this.searchEmail;
+
+    return this;
   }
   displayUser () {
+    console.log(this.tampil);
   }
 }
+const program1 = new Program(data);
+program1.findUser('spongebob@crustycrab.com').formatName().formatData().displayUser()
+
+
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
