0e162a706245e56965b09ff82fc908f6040308f1
Didit Aditya <email": null>
2017-03-30 17:38:17 +0700

    Upload method_chainring answer

--- method_chaining.js
+++ method_chaining.js
@@ -11,16 +11,31 @@ console.log(shout_backwards('Hello World'))
 class ShoutBackwards {
   // your code here
   constructor(words) {
+    this.string = words;
   }
   allcaps () {
+    this.string = this.string.toUpperCase();
+    return this;
   }
   splitting() {
+    this.string = this.string.split('');
+    return this;
   }
   reversing() {
+    this.string = this.string.reverse();;
+    return this;
   }
   backwards() {
+    this.string = this.string.join('');
+    return this;
   }
   addsuffix() {
+    this.string = this.string + "!!!";
+    return this;
+  }
+
+  print() {
+    console.log(this.string);
   }
 }
 
