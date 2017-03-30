6df2172f7f337f804db3fa1316adc7d183b405cc
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-03-30 22:16:23 +0700

    Method Chaining Selesai

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -18,28 +18,54 @@ let data = [{
 
 // Our object with the chainable methods using class in ES6
 class Program {
-  constructor () {
+  constructor (data) {
+    this.data = data;
+    this.firstName = "";
+    this.lastName = "";
+    this.lastName = "";
+    this.email = "";
+    this.id = "";
+    this.fullName = "";
+    this.formatDataBaru = "";
   }
   titleCaseName (string) {
   }
+
   findUser (email) {
+    for (var i = 0; i < this.data.length; i++) {
+      if(email === this.data[i].email){
+        this.firstName = this.data[i].firstName;
+        this.lastName = this.data[i].lastName;
+        this.email = this.data[i].email;
+        this.id = this.data[i].id;
+      }
+    }
+    return this;
   }
   formatName () {
+    this.fullName = this.firstName + ' ' + this.lastName;
+    return this;
   }
   formatData () {
+    this.formatDataBaru = 'Hasil : ' +this.fullName+
+                          '\nID : ' +this.id+
+                          '\nEmail : '+this.email;
+    return this;
   }
   displayUser () {
+    console.log(this.formatDataBaru);
   }
 }
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-const program = new Program()
-// program
-//   .findUser('spongebob@crustycrab.com')
-//   .formatName()
-//   .formatData()
-//   .displayUser()
+const program = new Program(data)
+// console.log(program.findUser('spongebob@crustycrab.com'));
+program
+  .findUser('spongebob@crustycrab.com')
+  .formatName()
+  .formatData()
+  .displayUser()
 
 // Hasil:
 // Member name: SpongeBob SquarePants
