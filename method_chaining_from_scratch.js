34f87db93da2b4ec4cb70672a4812bd79cb2bb6f
Bhirawa Mbani <bhirmbani@gmail.com>
2017-03-30 11:28:28 +0700

    method chaining first done

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -19,27 +19,52 @@ let data = [{
 // Our object with the chainable methods using class in ES6
 class Program {
   constructor () {
+    this.firstName;
+    this.lastName;
+    this.name;
+    this.email;
+    this.id;
+    this.userData;
   }
   titleCaseName (string) {
+    this.firstName = this.firstName.charAt(0).toUpperCase();
+    this.lastName = this.lastName.charAt(0).toUpperCase();
+    return this;
   }
   findUser (email) {
+    for(let i = 0; i < data.length; i++) {
+      if(data[i].email === email) {
+        this.firstName = data[i].firstName;
+        this.lastName = data[i].lastName;
+        this.email = data[i].email;
+        this.id = data[i].id;
+        return this;
+      }
+    }
   }
   formatName () {
+    this.name = `${this.firstName} ${this.lastName}`;
+    return this;
   }
   formatData () {
+    this.userData = `Member name: ${this.name}
+    ID: ${this.id}
+    Email: ${this.email}`;
+    return this;
   }
   displayUser () {
+    console.log(this.userData);
   }
 }
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
 const program = new Program()
-// program
-//   .findUser('spongebob@crustycrab.com')
-//   .formatName()
-//   .formatData()
-//   .displayUser()
+program
+  .findUser('spongebob@crustycrab.com')
+  .formatName()
+  .formatData()
+  .displayUser()
 
 // Hasil:
 // Member name: SpongeBob SquarePants
