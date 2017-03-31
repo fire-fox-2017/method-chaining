9be4ea789fb5e7ee8e43e880d8d5b0ebaccd6518
arahito <uarahito@yahoo.com>
2017-03-31 09:56:01 +0700

    Method Chaining

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -19,27 +19,48 @@ let data = [{
 // Our object with the chainable methods using class in ES6
 class Program {
   constructor () {
+    this.tempData = '';
+    this.fullname = '';
+    this.viewData = '';
   }
-  titleCaseName (string) {
-  }
+
+  // titleCaseName (string) {
+  //
+  // }
+
   findUser (email) {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].email === email) {
+        this.tempData = (data[i])
+        break
+      }
+    }
+    return this
   }
+
   formatName () {
+    this.fullname = `${this.tempData.firstName} ${this.tempData.lastName}`;
+    return this
   }
+
   formatData () {
+    this.viewData = `Member name: ${this.fullname} \nID: ${this.tempData.id} \nEmail: ${this.tempData.email}`;
+    return this
   }
+
   displayUser () {
+    console.log(`${this.viewData}`);
   }
 }
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
-const program = new Program()
-// program
-//   .findUser('spongebob@crustycrab.com')
-//   .formatName()
-//   .formatData()
-//   .displayUser()
+const program = new Program(data)
+program
+  .findUser('spongebob@crustycrab.com')
+  .formatName()
+  .formatData()
+  .displayUser()
 
 // Hasil:
 // Member name: SpongeBob SquarePants
