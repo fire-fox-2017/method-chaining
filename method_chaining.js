6ea0fba229af9ac3470f8c372c734cded9c7ef40
jamessraun <jeezybig@gmail.com>
2017-04-03 10:48:12 +0700

    first commit

--- method_chaining.js
+++ method_chaining.js
@@ -11,24 +11,40 @@ console.log(shout_backwards('Hello World'))
 class ShoutBackwards {
   // your code here
   constructor(words) {
+    this.words=words;
   }
   allcaps () {
+    this.words=this.words.toUpperCase();
+    return this;
   }
   splitting() {
+    this.words=this.words.split("");
+    return this;
   }
   reversing() {
+    this.words=this.words.reverse();
+    return this;
   }
   backwards() {
+    this.words=this.words.join("");
+    return this;
   }
   addsuffix() {
+    this.words=this.words+"!!!";
+    return this;
   }
+
+  print(){
+    console.log(this.words);
+  }
+
 }
 
 // -----------------------------------------------------------------------------
 // Kode di bawah ini merupakan driver code, jangan diubah ya
 
 var word = new ShoutBackwards('Hello World')
-word.allcaps().splitting().reversing().backwards().addsuffix().print()
+word.allcaps().splitting().reversing().backwards().addsuffix().print();
 
 // Alternatif penulisan:
 // word.allcaps().splitting().reversing().backwards().addsuffix().print()
